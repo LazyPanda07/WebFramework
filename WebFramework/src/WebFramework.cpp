145e302a7bb1c3915deee09fe75c48cdfad300cf
LazyPanda07 <semengricenko@gmail.com>
2020-12-02 16:20:55 +0500

    Added BaseTCPServer, removed x86 platform, added Dependencies solution folder

--- WebFramework/src/WebFramework.cpp
+++ WebFramework/src/WebFramework.cpp
@@ -1,5 +1,10 @@
 #include "WebFramework.h"
 
+#pragma comment (lib, "BaseTCPServer.lib")
+#pragma comment (lib, "HTTP.lib")
+#pragma comment (lib, "Log.lib")
+#pragma comment (lib, "SocketStreams.lib")
+
 namespace framework
 {
 
