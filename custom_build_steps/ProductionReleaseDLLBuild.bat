3bad332442aae0d4d9e598c537eb9bc8c13304b5
LazyPanda07 <semengricenko@gmail.com>
2020-12-24 01:51:31 +0500

    Added ProductionReleaseDLL configuration, ProductionReleaseDLLBuild.bat

---
+++ custom_build_steps/ProductionReleaseDLLBuild.bat
@@ -0,0 +1,32 @@
+@ECHO OFF
+
+CD ..
+
+SET libsFolder=%CD%\bin\ProductionReleaseDLL-x64\
+
+CD custom_build_steps
+CD utility
+
+CALL Setup.bat
+
+MKDIR ProductionReleaseDLL
+
+CD ProductionReleaseDLL
+
+MKDIR libs
+MKDIR dlls
+
+SET productionReleaseDLLFolder=%CD%\
+
+CD ..
+CD ..
+
+CD custom_build_steps
+CD utility
+
+CALL CopyHeaders.bat %productionReleaseDLLFolder%
+
+CALL CopyLibs.bat %libsFolder% %productionReleaseDLLFolder%libs
+
+COPY /Y web_framework_settings.ini %productionReleaseDLLFolder%
+COPY /Y %libsFolder%WebFramework\WebFramework.dll %productionReleaseDLLFolder%\dlls
