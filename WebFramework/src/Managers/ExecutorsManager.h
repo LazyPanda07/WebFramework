fb50cc89566745ed70b9aca3a7834e20a2cdb9b0
LazyPanda07 <semengricenko@gmail.com>
2020-12-29 01:29:07 +0500

    Added comments in ExecutorsManager

--- WebFramework/src/Managers/ExecutorsManager.h
+++ WebFramework/src/Managers/ExecutorsManager.h
@@ -16,6 +16,12 @@
 
 namespace framework
 {
+	/// <summary>
+	/// <para>Managing requests</para>
+	/// <para>Initializing subclasses of BaseStatefulExecutor</para>
+	/// <para>Holding pointer to ResourceExecutor</para>
+	/// <para>Initialize only through WebFramework</para>
+	/// </summary>
 	class ExecutorsManager
 	{
 	private:
@@ -36,13 +42,38 @@ namespace framework
 
 		ExecutorsManager& operator = (ExecutorsManager&& other) noexcept;
 
+		/// <summary>
+		/// Initialize method, called from WebFramework
+		/// </summary>
+		/// <param name="assets">path to assets</param>
+		/// <param name="isCaching">is caching resource files</param>
+		/// <param name="routes">routes for all executors</param>
+		/// <param name="creator">functions that create executors</param>
+		/// <param name="settings">parsed .xml file</param>
 		void init(const std::filesystem::path& assets, bool isCaching, std::unordered_map<std::string, std::unique_ptr<BaseExecutor>>&& routes, std::unordered_map<std::string, createBaseExecutorSubclassFunction>&& creator, std::unordered_map<std::string, utility::XMLSettingsParser::ExecutorSettings>&& settings) noexcept;
 
+		/// <summary>
+		/// Work in progress method
+		/// </summary>
+		/// <typeparam name="BaseExecutorSubclass">BaseExecutorSubclass type</typeparam>
+		/// <typeparam name="...Args">args type</typeparam>
+		/// <param name="route">request path</param>
+		/// <param name="...args">arguments for non-default constructor</param>
 		template<typename BaseExecutorSubclass, typename... Args>
 		void addRoute(const std::string& route, Args&&... args);
 
+		/// <summary>
+		/// Process requests from server
+		/// </summary>
+		/// <param name="request">from server</param>
+		/// <param name="response">for client</param>
+		/// <param name="statefulExecutors">all stateful executors for client</param>
 		void service(HTTPRequest&& request, HTTPResponse& response, std::unordered_map<std::string, std::unique_ptr<BaseExecutor>>& statefulExecutors);
 
+		/// <summary>
+		/// Getter for ResourceExecutor
+		/// </summary>
+		/// <returns>ResourceExecutor</returns>
 		std::unique_ptr<ResourceExecutor>& getResourceExecutor();
 
 		~ExecutorsManager() = default;
