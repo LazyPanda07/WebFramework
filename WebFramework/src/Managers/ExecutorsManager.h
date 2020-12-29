945e870d53848563dc9fd95e77eedb5189ce16d2
LazyPanda07 <semengricenko@gmail.com>
2020-12-30 02:37:08 +0500

    Deleted useless metho

--- WebFramework/src/Managers/ExecutorsManager.h
+++ WebFramework/src/Managers/ExecutorsManager.h
@@ -6,10 +6,6 @@
 #include <mutex>
 #include <memory>
 
-#include <type_traits>
-
-#include <stdexcept>
-
 #include "Executors/BaseExecutor.h"
 #include "Utility/XMLSettingsParser.h"
 #include "Executors/ResourceExecutor.h"
@@ -53,16 +49,6 @@ namespace framework
 		void init(const std::filesystem::path& assets, bool isCaching, std::unordered_map<std::string, std::unique_ptr<BaseExecutor>>&& routes, std::unordered_map<std::string, createBaseExecutorSubclassFunction>&& creator, std::unordered_map<std::string, utility::XMLSettingsParser::ExecutorSettings>&& settings) noexcept;
 
 		/// <summary>
-		/// Work in progress method
-		/// </summary>
-		/// <typeparam name="BaseExecutorSubclass">BaseExecutorSubclass type</typeparam>
-		/// <typeparam name="...Args">args type</typeparam>
-		/// <param name="route">request path</param>
-		/// <param name="...args">arguments for non-default constructor</param>
-		template<typename BaseExecutorSubclass, typename... Args>
-		void addRoute(const std::string& route, Args&&... args);
-
-		/// <summary>
 		/// Process requests from server
 		/// </summary>
 		/// <param name="request">from server</param>
@@ -78,12 +64,4 @@ namespace framework
 
 		~ExecutorsManager() = default;
 	};
-
-	template<typename BaseExecutorSubclass, typename... Args>
-	void ExecutorsManager::addRoute(const std::string& route, Args&&... args)
-	{
-		static_assert(std::is_base_of_v<BaseExecutor, BaseExecutorSubclass>, "BaseExecutorSubclass must be subclass of BaseExecutor");
-		
-		routes.insert(std::make_pair(route, std::make_unique<BaseExecutorSubclass>(std::forward<Args>(args)...)));
-	}
 }
