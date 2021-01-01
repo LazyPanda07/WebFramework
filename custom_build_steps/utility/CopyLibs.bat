f13ec9d12242c5f157f9cf115701b1101de3e83a
LazyPanda07 <semengricenko@gmail.com>
2021-01-02 00:49:08 +0500

    Added copying sqlite3.lib

--- custom_build_steps/utility/CopyLibs.bat
+++ custom_build_steps/utility/CopyLibs.bat
@@ -8,6 +8,9 @@ SET destinationLibsPath=%~2
 SET destinationDLLsPath=%~3
 
 FOR /R %sourcePath% %%i in (*.lib) DO COPY %%i %destinationLibsPath%
+
 FOR /R %sourcePath% %%i in (*.dll) DO COPY %%i %destinationDLLsPath%
 
-XCOPY /Y %sourcePath%WebFramework\WebFramework.pdb %destinationLibsPath%
+COPY /Y %sourcePath%WebFramework\WebFramework.pdb %destinationLibsPath%
+
+COPY /Y %CD%\WebFramework\libs\sqlite3.lib %destinationLibsPath%
