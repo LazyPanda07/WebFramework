9f2b13e2152e45782ae6a654924726cc73329a68
LazyPanda07 <semengricenko@gmail.com>
2021-01-09 16:45:34 +0500

    Refactor

--- WebFramework/src/Utility/StringConversion.cpp
+++ WebFramework/src/Utility/StringConversion.cpp
@@ -8,32 +8,64 @@ namespace framework
 {
 	namespace utility
 	{
-		wstring toUTF8(const string& source)
+		string toUTF8(const string& source, unsigned int sourceCodePage)
 		{
-			wstring result;
+			string result;
+			wstring tem;
 			int size = MultiByteToWideChar
 			(
-				CP_UTF8,
+				sourceCodePage,
 				NULL,
 				source.data(),
 				-1,
 				nullptr,
-				0
+				NULL
 			);
 			
-			result.resize(size - 1);
+			tem.resize(size - 1);
 
 			MultiByteToWideChar
 			(
-				CP_UTF8,
+				sourceCodePage,
 				NULL,
 				source.data(),
 				-1,
-				result.data(),
+				tem.data(),
 				size
 			);
 
+			size = WideCharToMultiByte
+			(
+				CP_UTF8,
+				NULL,
+				tem.data(),
+				-1,
+				nullptr,
+				NULL,
+				NULL,
+				NULL
+			);
+
+			result.resize(size - 1);
+
+			WideCharToMultiByte
+			(
+				CP_UTF8,
+				NULL,
+				tem.data(),
+				-1,
+				result.data(),
+				size,
+				NULL,
+				NULL
+			);
+
 			return result;
+		}
+
+		string cp1251ToUTF8(const string& source)
+		{
+			return toUTF8(source, 1251);
 		}
 	}
 }
--- WebFramework/src/Utility/StringConversion.h
+++ WebFramework/src/Utility/StringConversion.h
@@ -6,6 +6,19 @@ namespace framework
 {
 	namespace utility
 	{
-		std::wstring toUTF8(const std::string& source);
+		/// <summary>
+		/// Encode string to UTF8
+		/// </summary>
+		/// <param name="source">string to convert</param>
+		/// <param name="sourceCodePage">source encoding</param>
+		/// <returns>string in UTF8 encoding</returns>
+		std::string toUTF8(const std::string& source, unsigned int sourceCodePage);
+
+		/// <summary>
+		/// Encode string from CP1251 to UTF8
+		/// </summary>
+		/// <param name="source">string to convert</param>
+		/// <returns>string in UTF8 encoding</returns>
+		std::string cp1251ToUTF8(const std::string& source);
 	}
 }
