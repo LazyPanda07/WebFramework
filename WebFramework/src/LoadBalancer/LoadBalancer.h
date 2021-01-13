584aa52d9dee854a5d929d46ff648e1944f0805e
LazyPanda07 <semengricenko@gmail.com>
2021-01-14 01:47:12 +0500

    Added comments

--- WebFramework/src/LoadBalancer/LoadBalancer.h
+++ WebFramework/src/LoadBalancer/LoadBalancer.h
@@ -8,18 +8,38 @@ namespace framework
 {
 	namespace load_balancer
 	{
+		/// <summary>
+		/// <para>Main class of load balancer</para>
+		/// <para>All initializations go through this class</para>
+		/// </summary>
 		class LoadBalancer
 		{
 		private:
 			std::unique_ptr<LoadBalancerServer> loadBalancerServer;
 
 		public:
+			/// <summary>
+			/// Construct LoadBalancer
+			/// </summary>
+			/// <param name="configurationINIFile">path to .ini configuration file</param>
+			/// <exception cref="std::out_of_range">Can't find some settings</exception>
+			/// <exception cref="framework::exceptions::FileDoesNotExistException"></exception>
 			LoadBalancer(const std::filesystem::path& configurationINIFile);
 
+			/// <summary>
+			/// Start LoadBalancerServer server
+			/// </summary>
 			void startLoadBalancer();
 
+			/// <summary>
+			/// Stop LoadBalancerServer incoming connections
+			/// </summary>
 			void stopLoadBalancerServer();
 
+			/// <summary>
+			/// Is server running
+			/// </summary>
+			/// <returns>is server running</returns>
 			bool getLoadBalancerState() const;
 
 			~LoadBalancer() = default;
