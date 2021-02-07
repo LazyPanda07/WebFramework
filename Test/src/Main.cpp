98dc830abb23b79afc549d30e5a742f31470a358
LazyPanda07 <semengricenko@gmail.com>
2021-02-08 01:46:48 +0500

    Added Test

---
+++ Test/src/Main.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+
+#include "WebFramework.h"
+
+#pragma comment (lib, "WebFramework.lib")
+
+using namespace std;
+
+class Test : public framework::BaseStatelessExecutor
+{
+private:
+	vector<int64_t> numbers;
+
+public:
+	void init(const framework::utility::XMLSettingsParser::ExecutorSettings& settings) override
+	{
+		for (size_t i = 0; i < 5; i++)
+		{
+			numbers.push_back(rand());
+		}
+	}
+
+	void doGet(framework::HTTPRequest&& request, framework::HTTPResponse& response) override
+	{
+		response.addBody
+		(
+			json::JSONBuilder(1251).push_back<string>(make_pair("user"s, "admin")).push_back<string>(make_pair("password"s, "qweqwe123123"s)).push_back<vector<int64_t>>(make_pair("balance"s, numbers)).build()
+		);
+	}
+};
+
+CREATE_BASE_EXECUTOR_SUBCLASS_INSTANCE(Test)
+
+int main(int argc, char** argv)
+{
+	try
+	{
+		framework::WebFramework webFramework("web_framework_settings.ini");
+		string s;
+
+		webFramework.startServer();
+
+		while (cin >> s)
+		{
+
+		}
+	}
+	catch (const std::exception& e)
+	{
+		cout << e.what() << endl;
+	}
+
+	return 0;
+}
