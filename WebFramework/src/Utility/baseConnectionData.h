4cde8be13ab03fe929f25ea31dfadb86e76fcea4
LazyPanda07 <semengricenko@gmail.com>
2021-02-09 14:12:25 +0500

    Added MIDDLEWARE_DLL macro

--- WebFramework/src/Utility/baseConnectionData.h
+++ WebFramework/src/Utility/baseConnectionData.h
@@ -1,6 +1,15 @@
 #pragma once
 
+#if defined(MIDDLEWARE_DLL) || defined(ALL_DLL)
+#define WEB_FRAMEWORK_API __declspec(dllexport)
+#else
+#define WEB_FRAMEWORK_API
+#endif // MIDDLEWARE_DLL
+
+
 #include <string>
+
+#include "WebFrameworkConstants.h"
 
 namespace framework
 {
@@ -9,7 +18,7 @@ namespace framework
 		/// <summary>
 		/// Contains all data for connection to server
 		/// </summary>
-		struct baseConnectionData
+		struct WEB_FRAMEWORK_API baseConnectionData
 		{
 			std::string ip;
 			std::string port;
