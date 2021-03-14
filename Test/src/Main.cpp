8f5e8e6234c3a90a0e19ca0762454bfa0d16bfc6
LazyPanda07 <semengricenko@gmail.com>
2021-03-15 02:06:29 +0500

    Refactor

--- Test/src/Main.cpp
+++ Test/src/Main.cpp
@@ -1,10 +1,30 @@
 #include <iostream>
 
 #include "WebFramework.h"
+#include "SQLite3/SQLiteDatabaseModel.h"
 
 #pragma comment (lib, "WebFramework.lib")
 
 using namespace std;
+
+class Model : public framework::sqlite::SQLiteDatabaseModel
+{
+private:
+
+public:
+	Model() :
+		SQLiteDatabaseModel
+		(
+			"users",
+			framework::sqlite::SQLiteDatabase
+			(
+				"Database.sqlite3"
+			)
+		)
+	{
+		SQLiteDatabaseModel::createTableQuery({ { "id", "INTEGER PRIMARY KEY AUTOINCREMENT" }, { "login", "TEXT NOT NULL" } });
+	}
+};
 
 class Test : public framework::BaseStatelessExecutor
 {
@@ -16,7 +36,24 @@ public:
 
 	void doGet(framework::HTTPRequest&& request, framework::HTTPResponse& response) override
 	{
+		auto& model = request.getDatabaseModelInstance<Model>("Database.sqlite3", "users");
 
+		model->insertQuery({ { "login", "rofl" } });
+
+		framework::sqlite::utility::SQLiteResult result = model->selectAllQuery();
+
+		for (const auto& i : result)
+		{
+			for (const auto& j : i)
+			{
+				cout << "Column: " << j.first << '\t' << "Value: " << j.second << endl;
+			}
+		}
+
+		response.addBody
+		(
+			"<h1>Loaded</h1>"
+		);
 	}
 };
 
