ca705d015cf21fa493fefa2d461eb58ba048fea0
LazyPanda07 <semengricenko@gmail.com>
2021-04-08 12:19:24 +0500

    Add comments

--- WebFramework/src/WebNetwork/HTTPRequest.h
+++ WebFramework/src/WebNetwork/HTTPRequest.h
@@ -131,10 +131,18 @@ namespace framework
 		/// <param name="response">with file</param>
 		void sendAssetFile(const std::string& filePath, HTTPResponse& response, const smartPointer<std::unordered_map<std::string_view, std::string>>& variables = nullptr);
 
+		/// @brief Add new function in .wfdp interpreter
+		/// @param functionName Name of new function
+		/// @param function Function implementation
 		void registerDynamicFunction(const std::string& functionName, std::function<std::string(const std::vector<std::string>&)>&& function);
 
+		/// @brief Remove function from .wfdp interpreter
+		/// @param functionName Name of function
 		void unregisterDynamicFunction(const std::string& functionName);
 
+		/// @brief Check if function is registered
+		/// @param functionName Name of function
+		/// @return true if function is registered, false otherwise
 		bool isDynamicFunctionRegistered(const std::string& functionName);
 
 		/// <summary>
