0bfadd5cc4fa1ebff2d24be3e25bc5573a85bc09
LazyPanda07 <semengricenko@gmail.com>
2021-05-10 19:37:08 +0500

    Fixed

--- WebFramework/src/WebNetwork/WebServers/ThreadPoolWebServer.cpp
+++ WebFramework/src/WebNetwork/WebServers/ThreadPoolWebServer.cpp
@@ -174,6 +174,7 @@ namespace framework
 		SSL_CTX* context = nullptr;
 		utility::HTTPSSingleton& httpsSettings = utility::HTTPSSingleton::get();
 		bool useHTTPS = httpsSettings.getUseHTTPS();
+		u_long block = 0;
 
 		if (useHTTPS)
 		{
@@ -215,7 +216,7 @@ namespace framework
 			{
 				setsockopt(clientSocket, SOL_SOCKET, SO_RCVTIMEO, reinterpret_cast<const char*>(&timeout), sizeof(timeout));
 
-				ioctlsocket(clientSocket, FIONBIO, &blockingMode);
+				ioctlsocket(clientSocket, FIONBIO, &block);
 
 				data.insert(getClientIpV4(addr), clientSocket);
 
@@ -237,13 +238,15 @@ namespace framework
 						continue;
 					}
 
-					if (!SSL_accept(ssl))
+					if (SSL_accept(ssl) <= 0)
 					{
 						SSL_free(ssl);
 
 						continue;
 					}
 				}
+
+				ioctlsocket(clientSocket, FIONBIO, &blockingMode);
 
 				this->clientConnectionImplementation(clientSocket, addr, ssl, context);
 			}
