70f5be88fc56072b18046d4cb6040b9a05a37932
LazyPanda07 <semengricenko@gmail.com>
2022-10-04 12:38:49 +0500

    Move linking

--- WebFramework/src/WebFramework.h
+++ WebFramework/src/WebFramework.h
@@ -4,18 +4,6 @@
 
 #include "WebNetwork/WebServers/BaseWebServer.h"
 
-#pragma comment(lib, "BaseTCPServer.lib")
-#pragma comment(lib, "FileManager.lib")
-#pragma comment(lib, "HTTP.lib")
-#pragma comment(lib, "INIParser.lib")
-#pragma comment(lib, "JSON.lib")
-#pragma comment(lib, "Localization.lib")
-#pragma comment(lib, "Log.lib")
-#pragma comment(lib, "Networks.lib")
-#pragma comment(lib, "SHA256.lib")
-#pragma comment(lib, "SocketStreams.lib")
-#pragma comment(lib, "ThreadPool.lib")
-
 namespace framework
 {
 	/// <summary>
