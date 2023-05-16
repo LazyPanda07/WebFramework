fd9b900564d751d05a5d46421166b2e3f7aaf7c4
Semyon Gritsenko <40473939+LazyPanda07@users.noreply.github.com>
2023-05-16 16:40:33 +0500

    Fixes

--- Test/src/Main.cpp
+++ Test/src/Main.cpp
@@ -28,7 +28,7 @@ public:
 			{ "message"sv, helloMessage }
 		};
 
-		request.sendAssetFile("index.wfdp", response, arguments);
+		request.sendDynamicFile("index.wfdp", response, arguments);
 	}
 
 	~DefaultRoute() = default;
@@ -36,43 +36,12 @@ public:
 
 EXECUTOR_CREATION_FUNCTION(DefaultRoute)
 
-class AnotherRoute : public framework::BaseStatefulExecutor
-{
-public:
-	AnotherRoute()
-	{
-		cout << "Create AnotherRoute for user" << endl;
-	}
-
-	void doGet(framework::HTTPRequest&& request, framework::HTTPResponse& response) override
-	{
-		localization::TextLocalization& localization = localization::TextLocalization::get(); 
-		unordered_map<string_view, string> arguments =
-		{
-			{ "title"sv, localization["title"] },
-			{ "message"sv, localization["ipMessage"] + request.getClientIpV4()}
-		};
-
-		request.sendAssetFile("index.wfdp", response, arguments);
-	}
-
-	void destroy() override
-	{
-		cout << "Destroy AnotherRoute for user" << endl;
-	}
-
-	~AnotherRoute() = default;
-};
-
-EXECUTOR_CREATION_FUNCTION(AnotherRoute)
-
 int main(int argc, char** argv)
 {
 	try
 	{
 		framework::WebFramework webFramework("web_framework_settings.json");
 		string s;
-		localization::TextLocalization::get().changeLanguage("ru");
 
 		webFramework.startServer();
 
