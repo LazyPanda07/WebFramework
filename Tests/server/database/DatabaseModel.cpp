516832150706bf37d2d2ec3436c87de30bc3cae1
Semyon Gritsenko <40473939+LazyPanda07@users.noreply.github.com>
2024-05-04 19:52:18 +0500

    Fix

--- Tests/server/database/DatabaseModel.cpp
+++ Tests/server/database/DatabaseModel.cpp
@@ -1,1 +1,6 @@
 #include "DatabaseModel.h"
+
+DatabaseModel::DatabaseModel()
+{
+
+}
--- Tests/server/database/DatabaseModel.h
+++ Tests/server/database/DatabaseModel.h
@@ -7,7 +7,7 @@ class DatabaseModel : public framework::sqlite::SQLiteDatabaseModel
 	DECLARE_DATABASE_AND_TABLE_NAME("test_database", "test_table");
 
 public:
-	DatabaseModel() = default;
+	DatabaseModel();
 
 	~DatabaseModel() = default;
 };
