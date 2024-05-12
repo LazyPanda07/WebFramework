7e586b8d91c0029f76e7076a7671f97ce61d240a
Semyon Gritsenko <40473939+LazyPanda07@users.noreply.github.com>
2024-05-12 09:50:04 +0500

    Fix

--- Tests/scripts/run_memory_leak_tests.sh
+++ Tests/scripts/run_memory_leak_tests.sh
@@ -3,9 +3,6 @@
 set -e
 
 export WEB_FRAMEWORK_SERVER_CONFIG=$1
-export VALGRIND_RESULT_FREED=$(grep -Rnw 'memory_leaks_result.txt' -e 'All heap blocks were freed -- no leaks are possible')
-export VALGRIND_RESULT_DEFINITELY_LOST=$(grep -Rnw 'memory_leaks_result.txt' -e 'definitely lost: 0 bytes')
-export VALGRIND_RESULT_INDIRECTLY_LOST=$(grep -Rnw 'memory_leaks_result.txt' -e 'indirectly lost: 0 bytes')
 export LD_LIBRARY_PATH=$(pwd):${LD_LIBRARY_PATH}
 
 valgrind --max-threads=1000 --leak-check=full --show-leak-kinds=all --track-origins=yes --log-file=memory_leaks_result.txt ./Server ${WEB_FRAMEWORK_SERVER_CONFIG} & sleep 1
@@ -15,6 +12,10 @@ valgrind --max-threads=1000 --leak-check=full --show-leak-kinds=all --track-orig
 kill -TERM $(cat start_server.txt)
 
 sleep 10
+
+export VALGRIND_RESULT_FREED=$(grep -Rnw 'memory_leaks_result.txt' -e 'All heap blocks were freed -- no leaks are possible')
+export VALGRIND_RESULT_DEFINITELY_LOST=$(grep -Rnw 'memory_leaks_result.txt' -e 'definitely lost: 0 bytes')
+export VALGRIND_RESULT_INDIRECTLY_LOST=$(grep -Rnw 'memory_leaks_result.txt' -e 'indirectly lost: 0 bytes')
 
 cat memory_leaks_result.txt
 
