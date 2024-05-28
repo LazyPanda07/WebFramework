72aca7b0608d873834b31ee881e579b62b75eb4c
Semyon Gritsenko <40473939+LazyPanda07@users.noreply.github.com>
2024-05-28 17:01:35 +0500

    Fix

--- Tests/proxy/proxy_server/main.cpp
+++ Tests/proxy/proxy_server/main.cpp
@@ -4,19 +4,6 @@
 
 int main(int argc, char** argv) try
 {
-	// start ProxyServer.exe --config proxy_config.json --port 15000
-
-	argc = 5;
-	argv = new char*[argc];
-
-	std::string str = (std::filesystem::current_path() / "ProxyServer.exe").string();
-
-	argv[0] = str.data();
-	argv[1] = const_cast<char*>("--config");
-	argv[2] = const_cast<char*>("proxy_config.json");
-	argv[3] = const_cast<char*>("--port");
-	argv[4] = const_cast<char*>("15000");
-
 	utility::parsers::ConsoleArgumentParser parser(argc, argv);
 	framework::utility::Config config(parser.get<std::string>("--config"));
 
