89b8807c645323dd1f14c319ed5a3e6df3044b2b
Semyon Gritsenko <40473939+LazyPanda07@users.noreply.github.com>
2024-06-04 20:29:32 +0500

    Refactor

--- Tests/scripts/run_memory_leak_tests.sh
+++ Tests/scripts/run_memory_leak_tests.sh
@@ -2,10 +2,38 @@
 
 set -e
 
+function check_memory_leak_results()
+{
+	FILE_NAME=$1
+
+	VALGRIND_RESULT_FREED=$(grep -Rnw '${FILE_NAME}' -e 'All heap blocks were freed -- no leaks are possible')
+	VALGRIND_RESULT_DEFINITELY_LOST=$(grep -Rnw '${FILE_NAME}' -e 'definitely lost: 0 bytes')
+	VALGRIND_RESULT_INDIRECTLY_LOST=$(grep -Rnw '${FILE_NAME}' -e 'indirectly lost: 0 bytes')
+
+	cat ${FILE_NAME}.txt
+
+	if [[ -n "${VALGRIND_RESULT_DEFINITELY_LOST}" ]];
+	then
+		exit 0
+	fi
+	
+	if [[ -n "${VALGRIND_RESULT_INDIRECTLY_LOST}" ]];
+	then
+		exit 0
+	fi
+	
+	if [[ -z "${VALGRIND_RESULT_FREED}" ]];
+	then
+		exit 2
+	fi
+}
+
 export WEB_FRAMEWORK_SERVER_CONFIG=$1
 export LD_LIBRARY_PATH=$(pwd):${LD_LIBRARY_PATH}
 
-valgrind --max-threads=1000 --leak-check=full --show-leak-kinds=all --track-origins=yes --log-file=memory_leaks_result.txt ./Server ${WEB_FRAMEWORK_SERVER_CONFIG} & sleep 1
+CORE_FILE_NAME=core_memory_leaks_result.txt
+
+valgrind --max-threads=1000 --leak-check=full --show-leak-kinds=all --track-origins=yes --log-file=${CORE_FILE_NAME} ./Server ${WEB_FRAMEWORK_SERVER_CONFIG} & sleep 1
 
 ./Core ${WEB_FRAMEWORK_SERVER_CONFIG}
 
@@ -13,23 +41,4 @@ kill -TERM $(cat start_server.txt)
 
 sleep 10
 
-export VALGRIND_RESULT_FREED=$(grep -Rnw 'memory_leaks_result.txt' -e 'All heap blocks were freed -- no leaks are possible')
-export VALGRIND_RESULT_DEFINITELY_LOST=$(grep -Rnw 'memory_leaks_result.txt' -e 'definitely lost: 0 bytes')
-export VALGRIND_RESULT_INDIRECTLY_LOST=$(grep -Rnw 'memory_leaks_result.txt' -e 'indirectly lost: 0 bytes')
-
-cat memory_leaks_result.txt
-
-if [[ -n "${VALGRIND_RESULT_DEFINITELY_LOST}" ]];
-then
-	exit 0
-fi
-
-if [[ -n "${VALGRIND_RESULT_INDIRECTLY_LOST}" ]];
-then
-	exit 0
-fi
-
-if [[ -z "${VALGRIND_RESULT_FREED}" ]];
-then
-	exit 2
-fi
+check_memory_leak_results ${CORE_FILE_NAME}
