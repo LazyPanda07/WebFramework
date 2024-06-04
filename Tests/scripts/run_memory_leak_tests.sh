7169ad8ed361f4f30c9096b77714e17ad80581a2
Semyon Gritsenko <40473939+LazyPanda07@users.noreply.github.com>
2024-06-04 23:19:18 +0500

    Test

--- Tests/scripts/run_memory_leak_tests.sh
+++ Tests/scripts/run_memory_leak_tests.sh
@@ -5,23 +5,26 @@ set -e
 function check_memory_leak_results()
 {
 	local FILE_NAME=$1
-	local VALGRIND_RESULT_FREED=$(grep -Rnw '$(echo ${FILE_NAME})' -e 'All heap blocks were freed -- no leaks are possible')
-	local VALGRIND_RESULT_DEFINITELY_LOST=$(grep -Rnw '$(echo ${FILE_NAME})' -e 'definitely lost: 0 bytes')
-	local VALGRIND_RESULT_INDIRECTLY_LOST=$(grep -Rnw '$(echo ${FILE_NAME})' -e 'indirectly lost: 0 bytes')
+	# local VALGRIND_RESULT_FREED=$(grep -Rnw '$(echo ${FILE_NAME})' -e 'All heap blocks were freed -- no leaks are possible')
+	# local VALGRIND_RESULT_DEFINITELY_LOST=$(grep -Rnw '$(echo ${FILE_NAME})' -e 'definitely lost: 0 bytes')
+	# local VALGRIND_RESULT_INDIRECTLY_LOST=$(grep -Rnw '$(echo ${FILE_NAME})' -e 'indirectly lost: 0 bytes')
+	local VALGRIND_RESULT_FREED=$(cat ${FILE_NAME} | grep -Rnw -e 'All heap blocks were freed -- no leaks are possible')
+	local VALGRIND_RESULT_DEFINITELY_LOST=$(cat ${FILE_NAME} | grep -Rnw -e 'definitely lost: 0 bytes')
+	local VALGRIND_RESULT_INDIRECTLY_LOST=$(cat ${FILE_NAME} | grep -Rnw -e 'indirectly lost: 0 bytes')
 
 	cat ${FILE_NAME}
 
-	if [[ -n "$VALGRIND_RESULT_DEFINITELY_LOST" ]];
+	if [[ -n "${VALGRIND_RESULT_DEFINITELY_LOST}" ]];
 	then
 		exit 0
 	fi
 	
-	if [[ -n "$VALGRIND_RESULT_INDIRECTLY_LOST" ]];
+	if [[ -n "${VALGRIND_RESULT_INDIRECTLY_LOST}" ]];
 	then
 		exit 0
 	fi
 	
-	if [[ -z "$VALGRIND_RESULT_FREED" ]];
+	if [[ -z "${VALGRIND_RESULT_FREED}" ]];
 	then
 		exit 2
 	fi
