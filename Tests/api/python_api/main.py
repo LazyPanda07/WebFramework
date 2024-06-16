42ce623ad4aa7fd91684c4e33007b89d2f003283
Semyon Gritsenko <40473939+LazyPanda07@users.noreply.github.com>
2024-06-16 20:37:57 +0500

    Test

--- Tests/api/python_api/main.py
+++ Tests/api/python_api/main.py
@@ -1,4 +1,24 @@
 import sys
+import asyncio
+import subprocess
+
+from framework.Service import Service
+from framework.WebFrameworkException import WebFrameworkException
+
+
+async def start_server():
+    try:
+        service = Service("WebFramework")
+        server = service.create_web_framework(sys.argv[1])
+
+        server.start(True)
+    except WebFrameworkException as exception:
+        print(exception)
+
+        exit(-1)
+
 
 if __name__ == '__main__':
-    print(sys.argv[1])
+    asyncio.run(start_server())
+
+    exit(subprocess.run(["Core", sys.argv[1]]).returncode)
