e0ed9e532b3838f65dc56b7cdf82bb24772bc93f
Semyon Gritsenko <40473939+LazyPanda07@users.noreply.github.com>
2024-06-19 10:21:40 +0500

    Fix

--- Tests/api/python_api/server.py
+++ Tests/api/python_api/server.py
@@ -1,10 +1,14 @@
 import sys
+import os
 
 from framework.Service import Service
 from framework.WebFrameworkException import WebFrameworkException
 
 if __name__ == '__main__':
     try:
+        if os.path.exists("test_database.sqlite"):
+            os.remove("test_database.sqlite")
+
         service = Service("WebFramework")
         server = service.create_web_framework(sys.argv[1])
 
