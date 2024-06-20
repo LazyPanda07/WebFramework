29e4ca7e7ce2d28b51ac6826d818c33ae73d1c7e
Semyon Gritsenko <40473939+LazyPanda07@users.noreply.github.com>
2024-06-20 19:16:43 +0500

    Refactor

--- Tests/api/python_api/server.py
+++ Tests/api/python_api/server.py
@@ -4,6 +4,10 @@ import os
 from framework.Service import Service
 from framework.WebFrameworkException import WebFrameworkException
 
+def write_process_id():
+    with open("start_core_server.txt", "w") as file:
+            file.write(os.getpid())
+
 if __name__ == '__main__':
     try:
         if os.path.exists("test_database.sqlite"):
@@ -12,7 +16,7 @@ if __name__ == '__main__':
         service = Service("WebFramework")
         server = service.create_web_framework(sys.argv[1])
 
-        server.start(True)
+        server.start(True, write_process_id)
     except WebFrameworkException as exception:
         print(exception)
 
