f35ba2e44afe8eee343caea806be609949a1258b
Semyon Gritsenko <40473939+LazyPanda07@users.noreply.github.com>
2024-06-21 23:02:38 +0500

    Refactor

--- Tests/scripts/run_python_tests.bat
+++ Tests/scripts/run_python_tests.bat
@@ -3,15 +3,15 @@
 set WEB_FRAMEWORK_SERVER_CONFIG=%1
 
 start python server.py %WEB_FRAMEWORK_SERVER_CONFIG%
-REM start python proxy_server.py --config proxy_config.json --port 15000
-REM start python proxy_server.py --config proxy_config.json --port 15001 --useHTTPS
+start python proxy_server.py --config proxy_config.json --port 15000
+start python proxy_server.py --config proxy_config.json --port 15001 --useHTTPS
 REM start python load_balancer_server.py --config load_balancer_config.json --port 9090
 REM start python load_balancer_server.py --config load_balancer_config.json --port 9091 --serversHTTPS
 REM start python load_balancer_server.py --config load_balancer_config_https.json --port 9092
 REM start python load_balancer_server.py --config load_balancer_config_https.json --port 9093 --serversHTTPS
 REM start python load_balancer_server.py --config load_balancer_config.json --port 9094 --custom_heuristic
 
-REM start python load_balancer_server.py --config load_balancer_config.json --type server --port 10000
+start python load_balancer_server.py --config load_balancer_config.json --type server --port 10000
 REM start python load_balancer_server.py --config load_balancer_config.json --type server --port 10001 --serversHTTPS
 REM start python load_balancer_server.py --config load_balancer_config_https.json  --type server --port 10002
 REM start python load_balancer_server.py --config load_balancer_config_https.json --type server --port 10003 --serversHTTPS
@@ -24,5 +24,5 @@ REM call LoadBalancerCore.exe --port 9091 || exit 1
 REM call LoadBalancerCore.exe --port 9092 --useHTTPS || exit 1
 REM call LoadBalancerCore.exe --port 9093 --useHTTPS || exit 1
 REM call LoadBalancerCore.exe --port 9094 --custom_heuristic || exit 1
-REM call ProxyCore.exe --port 15000 || exit 1
-REM call ProxyCore.exe --port 15001 --useHTTPS || exit 1
+call ProxyCore.exe --port 15000 || exit 1
+call ProxyCore.exe --port 15001 --useHTTPS || exit 1
