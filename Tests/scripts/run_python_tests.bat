37093b110037789d289585d200d8615775192719
Semyon Gritsenko <40473939+LazyPanda07@users.noreply.github.com>
2024-06-21 23:09:06 +0500

    Refactor

--- Tests/scripts/run_python_tests.bat
+++ Tests/scripts/run_python_tests.bat
@@ -5,24 +5,24 @@ set WEB_FRAMEWORK_SERVER_CONFIG=%1
 start python server.py %WEB_FRAMEWORK_SERVER_CONFIG%
 start python proxy_server.py --config proxy_config.json --port 15000
 start python proxy_server.py --config proxy_config.json --port 15001 --useHTTPS
-REM start python load_balancer_server.py --config load_balancer_config.json --port 9090
-REM start python load_balancer_server.py --config load_balancer_config.json --port 9091 --serversHTTPS
-REM start python load_balancer_server.py --config load_balancer_config_https.json --port 9092
-REM start python load_balancer_server.py --config load_balancer_config_https.json --port 9093 --serversHTTPS
-REM start python load_balancer_server.py --config load_balancer_config.json --port 9094 --custom_heuristic
+start python load_balancer_server.py --config load_balancer_config.json --port 9090
+start python load_balancer_server.py --config load_balancer_config.json --port 9091 --serversHTTPS
+start python load_balancer_server.py --config load_balancer_config_https.json --port 9092
+start python load_balancer_server.py --config load_balancer_config_https.json --port 9093 --serversHTTPS
+start python load_balancer_server.py --config load_balancer_config.json --port 9094 --custom_heuristic
 
 start python load_balancer_server.py --config load_balancer_config.json --type server --port 10000
-REM start python load_balancer_server.py --config load_balancer_config.json --type server --port 10001 --serversHTTPS
-REM start python load_balancer_server.py --config load_balancer_config_https.json  --type server --port 10002
-REM start python load_balancer_server.py --config load_balancer_config_https.json --type server --port 10003 --serversHTTPS
+start python load_balancer_server.py --config load_balancer_config.json --type server --port 10001 --serversHTTPS
+start python load_balancer_server.py --config load_balancer_config_https.json  --type server --port 10002
+start python load_balancer_server.py --config load_balancer_config_https.json --type server --port 10003 --serversHTTPS
 
 start DefaultHTTPSServer.exe
 
 call Core.exe %WEB_FRAMEWORK_SERVER_CONFIG% || exit 1
-REM call LoadBalancerCore.exe --port 9090 || exit 1
-REM call LoadBalancerCore.exe --port 9091 || exit 1
-REM call LoadBalancerCore.exe --port 9092 --useHTTPS || exit 1
-REM call LoadBalancerCore.exe --port 9093 --useHTTPS || exit 1
-REM call LoadBalancerCore.exe --port 9094 --custom_heuristic || exit 1
+call LoadBalancerCore.exe --port 9090 || exit 1
+call LoadBalancerCore.exe --port 9091 || exit 1
+call LoadBalancerCore.exe --port 9092 --useHTTPS || exit 1
+call LoadBalancerCore.exe --port 9093 --useHTTPS || exit 1
+call LoadBalancerCore.exe --port 9094 --custom_heuristic || exit 1
 call ProxyCore.exe --port 15000 || exit 1
 call ProxyCore.exe --port 15001 --useHTTPS || exit 1
--- Tests/scripts/run_python_tests.sh
+++ Tests/scripts/run_python_tests.sh
@@ -8,25 +8,25 @@ export LD_LIBRARY_PATH=$(pwd):${LD_LIBRARY_PATH}
 python3 server.py ${WEB_FRAMEWORK_SERVER_CONFIG} &
 python3 proxy_server.py --config proxy_config.json --port 15000 &
 python3 proxy_server.py --config proxy_config.json --port 15001 --useHTTPS &
-# python3 load_balancer_server.py --config load_balancer_config.json --port 9090 &
-# python3 load_balancer_server.py --config load_balancer_config.json --port 9091 --serversHTTPS &
-# python3 load_balancer_server.py --config load_balancer_config_https.json --port 9092 &
-# python3 load_balancer_server.py --config load_balancer_config_https.json --port 9093 --serversHTTPS &
+python3 load_balancer_server.py --config load_balancer_config.json --port 9090 &
+python3 load_balancer_server.py --config load_balancer_config.json --port 9091 --serversHTTPS &
+python3 load_balancer_server.py --config load_balancer_config_https.json --port 9092 &
+python3 load_balancer_server.py --config load_balancer_config_https.json --port 9093 --serversHTTPS &
 
 python3 load_balancer_server.py --config load_balancer_config.json --type server --port 10000 &
-# python3 load_balancer_server.py --config load_balancer_config.json --type server --port 10001 --serversHTTPS &
-# python3 load_balancer_server.py --config load_balancer_config_https.json  --type server --port 10002 &
-# python3 load_balancer_server.py --config load_balancer_config_https.json --type server --port 10003 --serversHTTPS &
-# python3 load_balancer_server.py --config load_balancer_config.json --port 9094 --custom_heuristic &
+python3 load_balancer_server.py --config load_balancer_config.json --type server --port 10001 --serversHTTPS &
+python3 load_balancer_server.py --config load_balancer_config_https.json  --type server --port 10002 &
+python3 load_balancer_server.py --config load_balancer_config_https.json --type server --port 10003 --serversHTTPS &
+python3 load_balancer_server.py --config load_balancer_config.json --port 9094 --custom_heuristic &
 
 ./DefaultHTTPSServer &
 sleep 1
 
 ./Core ${WEB_FRAMEWORK_SERVER_CONFIG}
-# ./LoadBalancerCore --port 9090
-# ./LoadBalancerCore --port 9091
-# ./LoadBalancerCore --port 9092 --useHTTPS
-# ./LoadBalancerCore --port 9093 --useHTTPS
-# ./LoadBalancerCore --port 9094 --custom_heuristic
+./LoadBalancerCore --port 9090
+./LoadBalancerCore --port 9091
+./LoadBalancerCore --port 9092 --useHTTPS
+./LoadBalancerCore --port 9093 --useHTTPS
+./LoadBalancerCore --port 9094 --custom_heuristic
 ./ProxyCore --port 15000
 ./ProxyCore --port 15001 --useHTTPS
