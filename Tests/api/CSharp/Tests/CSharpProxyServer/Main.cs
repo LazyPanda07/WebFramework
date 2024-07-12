5075ad5409302920cce2aeda34fce46f4abdedd1
Semyon Gritsenko <40473939+LazyPanda07@users.noreply.github.com>
2024-07-12 18:14:40 +0500

    Add CSharpProxyServer

--- Tests/api/CSharp/Tests/CSharpProxyServer/Main.cs
+++ Tests/api/CSharp/Tests/CSharpProxyServer/Main.cs
@@ -2,11 +2,47 @@
 
 class ProxyServer
 {
-	static int Main()
+	static int Main(string config, long port, bool useHTTPS = false)
 	{
 		try
 		{
+			Config serverConfig = new(config);
 
+			serverConfig.OverrideConfiguration("useHTTPS", useHTTPS, true);
+			serverConfig.OverrideConfiguration("port", port, true);
+
+			WebFramework server = new(serverConfig);
+
+			server.Start
+			(
+				true,
+				() =>
+				{
+					FileStream file;
+
+					switch (port)
+					{
+						case 15000:
+							file = File.OpenWrite("start_proxy_server.txt");
+
+							break;
+
+						case 15001:
+							file = File.OpenWrite("start_proxy_https_server.txt");
+
+							break;
+
+						default:
+							return;
+					}
+
+					using StreamWriter writer = new(file);
+
+					writer.Write($"{Environment.ProcessId}");
+
+					file.Close();
+				}
+			);
 		}
 		catch (Exception exception)
 		{
