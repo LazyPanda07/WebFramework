262c63d7b936077a6a0e910449db8bc19d8dbaf7
Semyon Gritsenko <40473939+LazyPanda07@users.noreply.github.com>
2024-07-16 15:14:11 +0500

    Test

--- Tests/scripts/run_cc_tests.sh
+++ Tests/scripts/run_cc_tests.sh
@@ -9,17 +9,27 @@ export LD_LIBRARY_PATH=$(pwd):${LD_LIBRARY_PATH}
 ./DefaultHTTPSServer &
 ./CC_ProxyServer proxy_config.json 15000 &
 ./CC_ProxyServer proxy_config.json 15001 --useHTTPS &
-./CC_LoadBalancerServer load_balancer_config.json --port 9090 &
-./CC_LoadBalancerServer load_balancer_config.json --port 9091 --serversHTTPS &
-./CC_LoadBalancerServer load_balancer_config_https.json --port 9092 &
-./CC_LoadBalancerServer load_balancer_config_https.json --port 9093 --serversHTTPS &
+# ./CC_LoadBalancerServer load_balancer_config.json --port 9090 &
+# ./CC_LoadBalancerServer load_balancer_config.json --port 9091 --serversHTTPS &
+# ./CC_LoadBalancerServer load_balancer_config_https.json --port 9092 &
+# ./CC_LoadBalancerServer load_balancer_config_https.json --port 9093 --serversHTTPS &
 
-./CC_LoadBalancerServer load_balancer_config.json --type server --port 10000 &
-./CC_LoadBalancerServer load_balancer_config.json --type server --port 10001 --serversHTTPS &
-./CC_LoadBalancerServer load_balancer_config_https.json  --type server --port 10002 &
-./CC_LoadBalancerServer load_balancer_config_https.json --type server --port 10003 --serversHTTPS &
-./CC_LoadBalancerServer load_balancer_config.json --port 9094 --custom_heuristic &
+# ./CC_LoadBalancerServer load_balancer_config.json --type server --port 10000 &
+# ./CC_LoadBalancerServer load_balancer_config.json --type server --port 10001 --serversHTTPS &
+# ./CC_LoadBalancerServer load_balancer_config_https.json  --type server --port 10002 &
+# ./CC_LoadBalancerServer load_balancer_config_https.json --type server --port 10003 --serversHTTPS &
+# ./CC_LoadBalancerServer load_balancer_config.json --port 9094 --custom_heuristic &
 sleep 1
+
+./CC_LoadBalancerServer load_balancer_config.json --port 9090
+./CC_LoadBalancerServer load_balancer_config.json --port 9091 --serversHTTPS
+./CC_LoadBalancerServer load_balancer_config_https.json --port 9092
+./CC_LoadBalancerServer load_balancer_config_https.json --port 9093 --serversHTTPS
+./CC_LoadBalancerServer load_balancer_config.json --type server --port 10000
+./CC_LoadBalancerServer load_balancer_config.json --type server --port 10001 --serversHTTPS
+./CC_LoadBalancerServer load_balancer_config_https.json  --type server --port 10002
+./CC_LoadBalancerServer load_balancer_config_https.json --type server --port 10003 --serversHTTPS
+./CC_LoadBalancerServer load_balancer_config.json --port 9094 --custom_heuristic
 
 ./Core ${WEB_FRAMEWORK_SERVER_CONFIG}
 ./LoadBalancerCore --port 9090
