1ab870bbf6b52ae8e7f4e3debd8e70d2d59ebaff
LazyPanda07 <semengricenko@gmail.com>
2024-09-30 13:05:40 +0500

    Add distribution python test

---
+++ DistributionTests/python/main.py
@@ -0,0 +1,18 @@
+from web_framework_api.WebFramework import WebFramework  # Server
+from web_framework_api.utility.DLLHandler import initialize_web_framework  # WebFramework initialization
+from web_framework_api.exceptions.WebFrameworkException import WebFrameworkException  # Exception
+
+def on_start():
+  print("Server is running")
+
+if __name__ == '__main__':
+  try:
+    initialize_web_framework()
+
+    server = WebFramework.from_path("config.json")
+
+    server.start(True, on_start)
+  except WebFrameworkException as exception:
+    print(exception)
+
+    exit(-1)
