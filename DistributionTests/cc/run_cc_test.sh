cc6cc133ff4f3e23708ccf325f0981037b3fc2e8
LazyPanda07 <semengricenko@gmail.com>
2024-10-06 23:39:16 +0500

    Add CC distribution test

---
+++ DistributionTests/cc/run_cc_test.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -e
+
+export LD_LIBRARY_PATH=$(pwd):${LD_LIBRARY_PATH}
+
+./cc_test & sleep 1
+
+curl http://127.0.0.1:8080 > out.json
+
+md5sum reference.json
+md5sum out.json
--- FindWebFramework.cmake
+++ FindWebFramework.cmake
@@ -1,3 +1,17 @@
-add_definitions(-DWEB_FRAMEWORK_SDK=/lib/web-framework)
+if (NOT DEFINED WEB_FRAMEWORK_SDK)
+    set(WEB_FRAMEWORK_SDK $ENV{WEB_FRAMEWORK_SDK})
+endif()
 
-include(/lib/web-framework/WebFrameworkConfig.cmake)
+if (NOT DEFINED WEB_FRAMEWORK_SDK)
+    if (UNIX)
+        if (EXISTS /usr/lib/web-framework/WebFrameworkConfig.cmake)
+            set(WEB_FRAMEWORK_SDK /usr/lib/web-framework)
+        endif()
+    endif()
+endif()
+
+if (NOT DEFINED WEB_FRAMEWORK_SDK)
+    message(FATAL_ERROR "WEB_FRAMEWORK_SDK variable doesn't set")
+endif()
+
+include(${WEB_FRAMEWORK_SDK}/WebFrameworkConfig.cmake)
