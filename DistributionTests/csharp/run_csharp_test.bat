f4078f616cd881ccf9603bf5b59346b60d31c50b
LazyPanda07 <semengricenko@gmail.com>
2024-10-09 00:20:07 +0500

    Fix

--- DistributionTests/csharp/run_csharp_test.bat
+++ DistributionTests/csharp/run_csharp_test.bat
@@ -1,7 +1,15 @@
 @echo off
 
-dir
-
 start csharp_test.exe
 
 curl http://127.0.0.1:8080 > out.json
+
+fc reference.json out.json > nul
+if errorlevel 1 goto error
+else goto success
+
+:success
+exit 0
+
+:error
+exit 1
