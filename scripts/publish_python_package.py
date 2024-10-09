5e29e7c00c61b998cdb60c91d03d9c190b62056a
LazyPanda07 <semengricenko@gmail.com>
2024-10-09 23:35:49 +0500

    Fix

---
+++ scripts/publish_python_package.py
@@ -0,0 +1,34 @@
+import requests
+import os
+
+from pathlib import Path
+
+# PyPI API URL for uploads
+url = "https://upload.pypi.org/legacy/"
+
+# API token (replace this with your actual API token)
+api_token = os.environ["PYPI_API_KEY"]
+
+# Prepare the payload for the request
+data = {
+    ":action": "file_upload",
+}
+
+# Prepare headers
+headers = {
+    "Authorization": f"Basic {api_token}"
+}
+
+# Upload each file
+for path, _, file_name in os.walk("dist"):
+    with open(f"{path}/{file_name}", "rb") as file_content:
+        files = {"content": (file_name, file_content)}
+        response = requests.post(url, data=data, headers=headers, files=files)
+        
+        if response.status_code == 200:
+            print(f"Successfully uploaded {file_name} to PyPI.")
+        else:
+            print(f"Failed to upload {file_name}. Status code: {response.status_code}")
+            print(response.text)
+
+            exit(1)
