5564aed3e14b8f4d64bde4ff38591792f30de468
LazyPanda07 <semengricenko@gmail.com>
2025-02-14 10:59:20 +0500

    Fix demo

--- API/web_framework_flutter_api/android/src/main/kotlin/com/lazypanda07/web_framework_flutter_api/WebFrameworkFlutterApiPlugin.kt
+++ API/web_framework_flutter_api/android/src/main/kotlin/com/lazypanda07/web_framework_flutter_api/WebFrameworkFlutterApiPlugin.kt
@@ -10,7 +10,7 @@ import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
-import io.flutter.plugin.common.PluginRegistry.Registrar
+// import io.flutter.plugin.common.PluginRegistry.Registrar
 
 /** WebFrameworkFlutterApiPlugin */
 class WebFrameworkFlutterApiPlugin : FlutterPlugin, MethodCallHandler, ActivityAware {
