377c7ecf9d8982d0f55be313f81d477893d7638e
LazyPanda07 <semengricenko@gmail.com>
2025-03-03 18:59:39 +0500

    Fix

--- Tests/core/cases/ResourceUploading.cpp
+++ Tests/core/cases/ResourceUploading.cpp
@@ -4,6 +4,7 @@
 #include "gtest/gtest.h"
 
 #include "settings.h"
+#include "utilities.h"
 
 TEST(ResourceUploading, Chunk)
 {
@@ -19,9 +20,10 @@ TEST(ResourceUploading, OctetStream)
 {
 	constexpr std::string_view httpUrl = "http://127.0.0.1:8080/upload_octet_stream";
 	constexpr std::string_view httpsUrl = "https://127.0.0.1:8080/upload_octet_stream";
+	constexpr std::string_view uploadFileName = "octet_stream.bin";
 
-	int errorCode = std::system(std::format(R"(curl --insecure --header "Content-Type: application/octet-stream" --header "File-Name: octet_stream.bin" --data-binary @{} {})", LARGE_FILE_NAME, (useHTTPS ? httpsUrl : httpUrl)).data());
+	int errorCode = std::system(std::format(R"(curl --insecure --header "Content-Type: application/octet-stream" --header "File-Name: {}" --data-binary @{} {})", uploadFileName, LARGE_FILE_NAME, (useHTTPS ? httpsUrl : httpUrl)).data());
 
 	ASSERT_EQ(errorCode, 0);
-	ASSERT_TRUE(utility::compareFiles(LARGE_FILE_NAME, "octet_stream.bin"));
+	ASSERT_TRUE(utility::compareFiles(LARGE_FILE_NAME, uploadFileName));
 }
