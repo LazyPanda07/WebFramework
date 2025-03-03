acef9fdbc191263f80a62cb82b0a98828488ea13
LazyPanda07 <semengricenko@gmail.com>
2025-03-03 18:03:47 +0500

    Add --insecure flag

--- Tests/core/cases/ResourceUploading.cpp
+++ Tests/core/cases/ResourceUploading.cpp
@@ -20,7 +20,7 @@ TEST(ResourceUploading, OctetStream)
 	constexpr std::string_view httpUrl = "http://127.0.0.1:8080/upload_octet_stream";
 	constexpr std::string_view httpsUrl = "https://127.0.0.1:8080/upload_octet_stream";
 
-	int errorCode = std::system(std::format(R"(curl --header "Content-Type: application/octet-stream" --header "File-Name: octet_stream.bin" --data-binary @{} {})", LARGE_FILE_NAME, (useHTTPS ? httpsUrl : httpUrl)).data());
+	int errorCode = std::system(std::format(R"(curl --insecure --header "Content-Type: application/octet-stream" --header "File-Name: octet_stream.bin" --data-binary @{} {})", LARGE_FILE_NAME, (useHTTPS ? httpsUrl : httpUrl)).data());
 
 	ASSERT_EQ(errorCode, 0);
 }
