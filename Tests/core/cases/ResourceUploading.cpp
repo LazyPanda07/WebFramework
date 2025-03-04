789917af7896fea037100fddbafe50b6d7bee432
LazyPanda07 <semengricenko@gmail.com>
2025-03-04 16:34:46 +0500

    Fix

--- Tests/core/cases/ResourceUploading.cpp
+++ Tests/core/cases/ResourceUploading.cpp
@@ -16,15 +16,13 @@ TEST(ResourceUploading, Multipart)
 	constexpr std::string_view httpUrl = "http://127.0.0.1:8080/upload_multipart";
 	constexpr std::string_view httpsUrl = "https://127.0.0.1:8080/upload_multipart";
 	constexpr std::string_view firstFileName = "uploaded_api_test.py";
-	constexpr std::string_view secondFileName = "uploaded_sqlite3.dll";
-	constexpr std::string_view thirdFileName = "uploaded_web.json";
+	constexpr std::string_view secondFileName = "uploaded_web.json";
 
-	int errorCode = std::system(std::format(R"(curl --insecure -X POST -F "{}=@api_test.py;type=text/plain" -F "{}=@sqlite3.dll" -F "{}=@web.json;type=application/json" {})", firstFileName, secondFileName, thirdFileName, (useHTTPS ? httpsUrl : httpUrl)).data());
+	int errorCode = std::system(std::format(R"(curl --insecure -X POST -F "{}=@api_test.py;type=text/plain" -F "{}=@web.json;type=application/json" {})", firstFileName, secondFileName, (useHTTPS ? httpsUrl : httpUrl)).data());
 
 	ASSERT_EQ(errorCode, 0);
 	ASSERT_TRUE(utility::compareFiles(firstFileName, utility::removeUploadedPrefix(firstFileName)));
 	ASSERT_TRUE(utility::compareFiles(secondFileName, utility::removeUploadedPrefix(secondFileName)));
-	ASSERT_TRUE(utility::compareFiles(thirdFileName, utility::removeUploadedPrefix(thirdFileName)));
 }
 
 TEST(ResourceUploading, OctetStream)
@@ -33,7 +31,7 @@ TEST(ResourceUploading, OctetStream)
 	constexpr std::string_view httpsUrl = "https://127.0.0.1:8080/upload_octet_stream";
 	constexpr std::string_view uploadFileName = "octet_stream.bin";
 
-	int errorCode = std::system(std::format(R"(curl --insecure --header "Content-Type: application/octet-stream" --header "File-Name: {}" --header "Expect: nothing" --data-binary @{} {})", uploadFileName, LARGE_FILE_NAME, (useHTTPS ? httpsUrl : httpUrl)).data());
+	int errorCode = std::system(std::format(R"(curl --insecure --header "Content-Type: application/octet-stream" --header "File-Name: {}" --data-binary @{} {})", uploadFileName, LARGE_FILE_NAME, (useHTTPS ? httpsUrl : httpUrl)).data());
 
 	ASSERT_EQ(errorCode, 0);
 	ASSERT_TRUE(utility::compareFiles(LARGE_FILE_NAME, uploadFileName));
