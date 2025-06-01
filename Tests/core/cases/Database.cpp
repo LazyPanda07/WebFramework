50772de2930aebf5668951f11d22b53cfdfebedd
LazyPanda07 <semengricenko@gmail.com>
2025-06-01 16:42:10 +0500

    Fix

--- Tests/core/cases/Database.cpp
+++ Tests/core/cases/Database.cpp
@@ -6,69 +6,67 @@
 
 #include "utilities.h"
 
-// TODO: Fix
+TEST(Database, Create)
+{
+	streams::IOSocketStream stream = utility::createSocketStream();
+	std::string request = web::HTTPBuilder().postRequest().parameters("database").build();
+	std::string response;
 
-//TEST(Database, Create)
-//{
-//	streams::IOSocketStream stream = utility::createSocketStream();
-//	std::string request = web::HTTPBuilder().postRequest().parameters("database").build();
-//	std::string response;
-//
-//	stream << request;
-//
-//	stream >> response;
-//
-//	ASSERT_EQ(web::HTTPParser(response).getResponseCode(), web::ResponseCodes::ok) << response;
-//}
-//
-//TEST(Database, Insert)
-//{
-//	streams::IOSocketStream stream = utility::createSocketStream();
-//	std::string request = web::HTTPBuilder().putRequest().parameters("database").build();
-//	std::string response;
-//
-//	stream << request;
-//
-//	stream >> response;
-//
-//	ASSERT_EQ(web::HTTPParser(response).getResponseCode(), web::ResponseCodes::ok) << response;
-//}
-//
-//TEST(Database, Select)
-//{
-//	streams::IOSocketStream stream = utility::createSocketStream();
-//	std::string request = web::HTTPBuilder().getRequest().parameters("database").build();
-//	std::string response;
-//
-//	stream << request;
-//
-//	stream >> response;
-//
-//	ASSERT_EQ(web::HTTPParser(response).getJSON().getArray("data").size(), 11) << response;
-//}
-//
-//TEST(Database, Update)
-//{
-//	streams::IOSocketStream stream = utility::createSocketStream();
-//	std::string request = web::HTTPBuilder().patchRequest().parameters("database").build();
-//	std::string response;
-//
-//	stream << request;
-//
-//	stream >> response;
-//
-//	ASSERT_EQ(web::HTTPParser(response).getJSON().getArray("data").size(), 1) << response;
-//}
-//
-//TEST(Database, Delete)
-//{
-//	streams::IOSocketStream stream = utility::createSocketStream();
-//	std::string request = web::HTTPBuilder().getRequest().parameters("database").build();
-//	std::string response;
-//
-//	stream << request;
-//
-//	stream >> response;
-//
-//	ASSERT_EQ(web::HTTPParser(response).getResponseCode(), web::ResponseCodes::ok) << response;
-//}
+	stream << request;
+
+	stream >> response;
+
+	ASSERT_EQ(web::HTTPParser(response).getResponseCode(), web::ResponseCodes::ok) << response;
+}
+
+TEST(Database, Insert)
+{
+	streams::IOSocketStream stream = utility::createSocketStream();
+	std::string request = web::HTTPBuilder().putRequest().parameters("database").build();
+	std::string response;
+
+	stream << request;
+
+	stream >> response;
+
+	ASSERT_EQ(web::HTTPParser(response).getResponseCode(), web::ResponseCodes::ok) << response;
+}
+
+TEST(Database, Select)
+{
+	streams::IOSocketStream stream = utility::createSocketStream();
+	std::string request = web::HTTPBuilder().getRequest().parameters("database").build();
+	std::string response;
+
+	stream << request;
+
+	stream >> response;
+
+	ASSERT_EQ(web::HTTPParser(response).getJSON().getArray("data").size(), 11) << response;
+}
+
+TEST(Database, Update)
+{
+	streams::IOSocketStream stream = utility::createSocketStream();
+	std::string request = web::HTTPBuilder().patchRequest().parameters("database").build();
+	std::string response;
+
+	stream << request;
+
+	stream >> response;
+
+	ASSERT_EQ(web::HTTPParser(response).getJSON().getArray("data").size(), 1) << response;
+}
+
+TEST(Database, Delete)
+{
+	streams::IOSocketStream stream = utility::createSocketStream();
+	std::string request = web::HTTPBuilder().getRequest().parameters("database").build();
+	std::string response;
+
+	stream << request;
+
+	stream >> response;
+
+	ASSERT_EQ(web::HTTPParser(response).getResponseCode(), web::ResponseCodes::ok) << response;
+}
