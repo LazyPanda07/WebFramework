83a7ca4869489a8be8f5a2240d0237480422759e
LazyPanda07 <semengricenko@gmail.com>
2025-06-01 18:17:48 +0500

    Fix

--- Tests/server/database/CRUDExecutor.cpp
+++ Tests/server/database/CRUDExecutor.cpp
@@ -52,7 +52,7 @@ void CRUDExecutor::doPut(framework::HTTPRequest& request, framework::HTTPRespons
 	{
 		table.execute
 		(
-			"INSERT INTO test_table VALUES(name, amount) VALUES(?, ?)",
+			"INSERT INTO test_table (name, amount) VALUES(?, ?)",
 			{ framework::SQLValue("glue"), framework::SQLValue(std::to_string(random() % 200)) }
 		);
 	}
