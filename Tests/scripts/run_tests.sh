805e3fd9f68b1539a840de9cb4b07bb152cce0e3
LazyPanda07 <semengricenko@gmail.com>
2025-06-15 15:39:51 +0500

    Add ADDITIONAL_ARGS

--- Tests/scripts/run_tests.sh
+++ Tests/scripts/run_tests.sh
@@ -5,27 +5,31 @@ set -e
 export WEB_FRAMEWORK_SERVER_CONFIG=$1
 export LD_LIBRARY_PATH=$(pwd):${LD_LIBRARY_PATH}
 
-./Server ${WEB_FRAMEWORK_SERVER_CONFIG} &
-./DefaultHTTPSServer &
-./ProxyServer --config proxy_config.json --port 15000 &
-./ProxyServer --config proxy_config.json --port 15001 --useHTTPS &
-./LoadBalancerServer --config load_balancer_config.json --port 9090 &
-./LoadBalancerServer --config load_balancer_config.json --port 9091 --serversHTTPS &
-./LoadBalancerServer --config load_balancer_config_https.json --port 9092 &
-./LoadBalancerServer --config load_balancer_config_https.json --port 9093 --serversHTTPS &
-./LoadBalancerServer --config load_balancer_config.json --port 9094 --custom_heuristic &
+if [ -z "${ADDITIONAL_ARGS+x}" ]; then
+    ADDITIONAL_ARGS=""
+fi
 
-./LoadBalancerServer --config load_balancer_config.json --type server --port 10000 &
-./LoadBalancerServer --config load_balancer_config.json --type server --port 10001 --serversHTTPS &
-./LoadBalancerServer --config load_balancer_config_https.json  --type server --port 10002 &
-./LoadBalancerServer --config load_balancer_config_https.json --type server --port 10003 --serversHTTPS &
+${ADDITIONAL_ARGS} ./Server ${WEB_FRAMEWORK_SERVER_CONFIG} &
+${ADDITIONAL_ARGS} ./DefaultHTTPSServer &
+${ADDITIONAL_ARGS} ./ProxyServer --config proxy_config.json --port 15000 &
+${ADDITIONAL_ARGS} ./ProxyServer --config proxy_config.json --port 15001 --useHTTPS &
+${ADDITIONAL_ARGS} ./LoadBalancerServer --config load_balancer_config.json --port 9090 &
+${ADDITIONAL_ARGS} ./LoadBalancerServer --config load_balancer_config.json --port 9091 --serversHTTPS &
+${ADDITIONAL_ARGS} ./LoadBalancerServer --config load_balancer_config_https.json --port 9092 &
+${ADDITIONAL_ARGS} ./LoadBalancerServer --config load_balancer_config_https.json --port 9093 --serversHTTPS &
+${ADDITIONAL_ARGS} ./LoadBalancerServer --config load_balancer_config.json --port 9094 --custom_heuristic &
+
+${ADDITIONAL_ARGS} ./LoadBalancerServer --config load_balancer_config.json --type server --port 10000 &
+${ADDITIONAL_ARGS} ./LoadBalancerServer --config load_balancer_config.json --type server --port 10001 --serversHTTPS &
+${ADDITIONAL_ARGS} ./LoadBalancerServer --config load_balancer_config_https.json  --type server --port 10002 &
+${ADDITIONAL_ARGS} ./LoadBalancerServer --config load_balancer_config_https.json --type server --port 10003 --serversHTTPS &
 sleep 1
 
-./Core ${WEB_FRAMEWORK_SERVER_CONFIG}
-./LoadBalancerCore --port 9090
-./LoadBalancerCore --port 9091
-./LoadBalancerCore --port 9092 --useHTTPS
-./LoadBalancerCore --port 9093 --useHTTPS
-./LoadBalancerCore --port 9094 --custom_heuristic
-./ProxyCore --port 15000
-./ProxyCore --port 15001 --useHTTPS
+${ADDITIONAL_ARGS} ./Core ${WEB_FRAMEWORK_SERVER_CONFIG}
+${ADDITIONAL_ARGS} ./LoadBalancerCore --port 9090
+${ADDITIONAL_ARGS} ./LoadBalancerCore --port 9091
+${ADDITIONAL_ARGS} ./LoadBalancerCore --port 9092 --useHTTPS
+${ADDITIONAL_ARGS} ./LoadBalancerCore --port 9093 --useHTTPS
+${ADDITIONAL_ARGS} ./LoadBalancerCore --port 9094 --custom_heuristic
+${ADDITIONAL_ARGS} ./ProxyCore --port 15000
+${ADDITIONAL_ARGS} ./ProxyCore --port 15001 --useHTTPS
