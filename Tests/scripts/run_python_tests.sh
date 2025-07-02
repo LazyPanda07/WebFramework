9a123d0f485096e4f54c419cf88cf17334fb6cb8
LazyPanda07 <semengricenko@gmail.com>
2025-07-02 23:40:30 +0500

    Test

--- Tests/scripts/run_python_tests.sh
+++ Tests/scripts/run_python_tests.sh
@@ -6,15 +6,15 @@ export WEB_FRAMEWORK_SERVER_CONFIG=$1
 export LD_LIBRARY_PATH=$(pwd):${LD_LIBRARY_PATH}
 
 python3 server.py ${WEB_FRAMEWORK_SERVER_CONFIG} &
-# python3 proxy_server.py --config proxy_config.json --port 15000 &
-# python3 proxy_server.py --config proxy_config.json --port 15001 --useHTTPS &
+python3 proxy_server.py --config proxy_config.json --port 15000 &
+python3 proxy_server.py --config proxy_config.json --port 15001 --useHTTPS &
 # python3 load_balancer_server.py --config load_balancer_config.json --port 9090 &
 # python3 load_balancer_server.py --config load_balancer_config.json --port 9091 --serversHTTPS &
 # python3 load_balancer_server.py --config load_balancer_config_https.json --port 9092 &
 # python3 load_balancer_server.py --config load_balancer_config_https.json --port 9093 --serversHTTPS &
 # 
-# python3 load_balancer_server.py --config load_balancer_config.json --type server --port 10000 &
-# python3 load_balancer_server.py --config load_balancer_config.json --type server --port 10001 --serversHTTPS &
+python3 load_balancer_server.py --config load_balancer_config.json --type server --port 10000 &
+python3 load_balancer_server.py --config load_balancer_config.json --type server --port 10001 --serversHTTPS &
 # python3 load_balancer_server.py --config load_balancer_config_https.json  --type server --port 10002 &
 # python3 load_balancer_server.py --config load_balancer_config_https.json --type server --port 10003 --serversHTTPS &
 # python3 load_balancer_server.py --config load_balancer_config.json --port 9094 --custom_heuristic &
@@ -29,5 +29,5 @@ python3 api_test.py
 # ./LoadBalancerCore --port 9092 --useHTTPS
 # ./LoadBalancerCore --port 9093 --useHTTPS
 # ./LoadBalancerCore --port 9094 --custom_heuristic
-# ./ProxyCore --port 15000
-# ./ProxyCore --port 15001 --useHTTPS
+./ProxyCore --port 15000
+./ProxyCore --port 15001 --useHTTPS
