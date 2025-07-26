907f39cb9c905ccddcf03c50e32161526cca4337
LazyPanda07 <semengricenko@gmail.com>
2025-07-26 15:38:09 +0500

    Fix samples

--- samples/VisualStudioSample/Executors/src/HelloExecutor.cpp
+++ samples/VisualStudioSample/Executors/src/HelloExecutor.cpp
@@ -1,10 +1,8 @@
 #include "HelloExecutor.h"
 
-#include "JSONBuilder.h"
-
 void HelloExecutor::doGet(framework::HTTPRequest& request, framework::HTTPResponse& response)
 {
-	response.setBody(json::JSONBuilder(CP_UTF8).appendString("message", "Hello, World!"));
+	response.setBody(framework::JSONBuilder().append("message", "Hello, World!"));
 }
 
 DECLARE_EXECUTOR(HelloExecutor)
--- samples/hello_executor/source/HelloExecutor.cpp
+++ samples/hello_executor/source/HelloExecutor.cpp
@@ -1,12 +1,10 @@
 #include "HelloExecutor.h"
 
-#include "JSONBuilder.h"
-
 namespace executors
 {
 	void HelloExecutor::doGet(framework::HTTPRequest& request, framework::HTTPResponse& response)
 	{
-		response.setBody(json::JSONBuilder(CP_UTF8).appendString("message", "Hello, World!"));
+		response.setBody(framework::JSONBuilder().append("message", "Hello, World!"));
 	}
 
 	DECLARE_EXECUTOR(HelloExecutor);
