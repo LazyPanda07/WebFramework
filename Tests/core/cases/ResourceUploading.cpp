620f5e1f27e6777cb40c26af95a67596ecbd5548
Semyon Gritsenko <40473939+LazyPanda07@users.noreply.github.com>
2025-08-13 16:05:26 +0500

    Add curl timeout

--- Tests/core/cases/ResourceUploading.cpp
+++ Tests/core/cases/ResourceUploading.cpp
@@ -14,7 +14,7 @@ TEST(ResourceUploading, Chunk)
 	constexpr std::string_view httpsUrl = "https://127.0.0.1:8080/upload_chunked";
 	constexpr std::string_view uploadFileName = "load_balancer_server.py";
 
-	int errorCode = std::system(std::format(R"(curl --insecure --header "Transfer-Encoding: chunked" --header "File-Name: uploaded_{}" --data-binary @{} {})", uploadFileName, uploadFileName, (useHTTPS ? httpsUrl : httpUrl)).data());
+	int errorCode = std::system(std::format(R"(curl --max-time 3600 --insecure --header "Transfer-Encoding: chunked" --header "File-Name: uploaded_{}" --data-binary @{} {})", uploadFileName, uploadFileName, (useHTTPS ? httpsUrl : httpUrl)).data());
 
 	ASSERT_EQ(errorCode, 0);
 	ASSERT_TRUE(utility::compareFiles(std::format("uploaded_{}", uploadFileName), uploadFileName));
@@ -27,7 +27,7 @@ TEST(ResourceUploading, Multipart)
 	constexpr std::string_view firstFileName = "uploaded_api_test.py";
 	constexpr std::string_view secondFileName = "uploaded_web.json";
 
-	int errorCode = std::system(std::format(R"(curl --insecure -X POST -F "{}=@api_test.py;type=text/plain" -F "{}=@web.json;type=application/json" {})", firstFileName, secondFileName, (useHTTPS ? httpsUrl : httpUrl)).data());
+	int errorCode = std::system(std::format(R"(curl --max-time 3600 --insecure -X POST -F "{}=@api_test.py;type=text/plain" -F "{}=@web.json;type=application/json" {})", firstFileName, secondFileName, (useHTTPS ? httpsUrl : httpUrl)).data());
 
 	ASSERT_EQ(errorCode, 0);
 	ASSERT_TRUE(utility::compareFiles(firstFileName, utility::removeUploadedPrefix(firstFileName)));
@@ -45,7 +45,7 @@ TEST(ResourceUploading, OctetStream)
 
 	uintmax_t fileSize = std::filesystem::file_size(LARGE_FILE_NAME);
 	streams::IOSocketStream stream = utility::createSocketStream();
-	int errorCode = std::system(std::format(R"(curl --insecure -X POST {} -H "Content-Type: application/octet-stream" -H "File-Name: {}" --data-binary @{})", (useHTTPS ? httpsUrl : httpUrl), uploadFileName, LARGE_FILE_NAME).data());
+	int errorCode = std::system(std::format(R"(curl --max-time 3600 --insecure -X POST {} -H "Content-Type: application/octet-stream" -H "File-Name: {}" --data-binary @{})", (useHTTPS ? httpsUrl : httpUrl), uploadFileName, LARGE_FILE_NAME).data());
 
 	ASSERT_EQ(errorCode, 0);
 	ASSERT_TRUE(utility::compareFiles(LARGE_FILE_NAME, uploadFileName));
