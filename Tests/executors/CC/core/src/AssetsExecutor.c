a1efd4461c55938d87789ca474774d282a689478
LazyPanda07 <semengricenko@gmail.com>
2025-08-24 11:01:12 +0500

    Fix AssetsExecutor

--- Tests/executors/CC/core/src/AssetsExecutor.c
+++ Tests/executors/CC/core/src/AssetsExecutor.c
@@ -7,14 +7,14 @@ DECLARE_DEFAULT_EXECUTOR(AssetsExecutor, STATELESS_EXECUTOR);
 static const char* customFunction(const char** args, size_t agumentsNumber)
 {
 	size_t bufferSize = 1024;
-	const char* buffer = (const char*)malloc(bufferSize);
+	const char* buffer = (const char*)calloc(bufferSize, sizeof(char));
 
 	if (!buffer)
 	{
 		raise(SIGSEGV);
-	}
 
-	memset(buffer, 0, bufferSize);
+		return "";
+	}
 
 	snprintf(buffer, bufferSize, "Data: %s %s %s", args[0], args[1], args[2]);
 
@@ -30,7 +30,7 @@ DECLARE_EXECUTOR_METHOD(AssetsExecutor, GET_METHOD, request, response)
 {
 	JSONParser parser;
 	const char* fileName;
-	const char extension[] = ".wfdp";
+	const char extension[] = "wfdp";
 	QueryParameter* queryParameters;
 	size_t queryParametersSize;
 
@@ -41,8 +41,28 @@ DECLARE_EXECUTOR_METHOD(AssetsExecutor, GET_METHOD, request, response)
 
 	DynamicPagesVariable* variables = (DynamicPagesVariable*)malloc(queryParametersSize * sizeof(DynamicPagesVariable));
 
-	size_t fullNameSize = strlen(fileName) + strlen(extension);
-	const char* fullName = (const char*)malloc(fullNameSize);
+	if (!variables)
+	{
+		raise(SIGSEGV);
+
+		return "";
+	}
+
+	for (size_t i = 0; i < queryParametersSize; i++)
+	{
+		variables[i].name = queryParameters[i].key;
+		variables[i].value = queryParameters[i].value;
+	}
+
+	size_t fullNameSize = strlen(fileName) + strlen(extension) + 1;
+	const char* fullName = (const char*)calloc(fullNameSize, sizeof(char));
+
+	if (!fullName)
+	{
+		raise(SIGSEGV);
+
+		return "";
+	}
 
 	snprintf(fullName, fullNameSize, "%s.%s", fileName, extension);
 
