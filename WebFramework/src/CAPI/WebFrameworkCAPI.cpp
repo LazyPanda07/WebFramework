4c39e51e8f33efad0bf57539a24686215e1be59f
LazyPanda07 <semengricenko@gmail.com>
2025-09-13 14:13:34 +0500

    Add additional information in unexpected error

--- WebFramework/src/CAPI/WebFrameworkCAPI.cpp
+++ WebFramework/src/CAPI/WebFrameworkCAPI.cpp
@@ -1,6 +1,7 @@
 #include "WebFrameworkCAPI.h"
 
 #include <fstream>
+#include <format>
 
 #include "Framework/WebFramework.h"
 #include "Framework/Config.h"
@@ -11,7 +12,7 @@
 #define LOG_EXCEPTION() if (Log::isValid()) { Log::error("Exception: {}", "C_API", e.what()); }
 #define CREATE_EXCEPTION() *exception = new std::runtime_error(e.what())
 #define LOG_AND_CREATE_EXCEPTION() LOG_EXCEPTION(); CREATE_EXCEPTION()
-#define UNEXPECTED_EXCEPTION() if (Log::isValid()) { Log::error("Somethind went wrong", "C_API"); } *exception = new std::runtime_error("Something went wrong"); std::ofstream("error.txt") << "Something went wrong"
+#define UNEXPECTED_EXCEPTION() if (Log::isValid()) { Log::error("Somethind went wrong", "C_API"); } *exception = new std::runtime_error(std::format("Something went wrong in file: {} on line: {}", __FILE__, __LINE__));
 
 const char* getDataFromString(String string)
 {
--- WebFramework/src/CAPI/WebFrameworkExecutorsCAPI.cpp
+++ WebFramework/src/CAPI/WebFrameworkExecutorsCAPI.cpp
@@ -9,7 +9,7 @@
 #define LOG_EXCEPTION() if (Log::isValid()) { Log::error("Exception: {}", "C_API", e.what()); }
 #define CREATE_EXCEPTION() *exception = new std::runtime_error(e.what())
 #define LOG_AND_CREATE_EXCEPTION() LOG_EXCEPTION(); CREATE_EXCEPTION()
-#define UNEXPECTED_EXCEPTION() if (Log::isValid()) { Log::error("Somethind went wrong", "C_API"); } *exception = new std::runtime_error("Something went wrong");
+#define UNEXPECTED_EXCEPTION() if (Log::isValid()) { Log::error("Somethind went wrong", "C_API"); } *exception = new std::runtime_error(std::format("Something went wrong in file: {} on line: {}", __FILE__, __LINE__));
 
 void setHTTPResponseBody(HTTPResponseObject response, const char* body, Exception* exception)
 {
