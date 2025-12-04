5a2386b629612b8fd95828ab741ecb328a65fdd9
LazyPanda07 <semengricenko@gmail.com>
2025-12-04 21:46:25 +0500

    Add custom heuristic, append windows-tests, linux-tests, remove csharp tests

--- Tests/executors/CSharp/LoadBalancerExecutors/CustomHeuristic.cs
+++ Tests/executors/CSharp/LoadBalancerExecutors/CustomHeuristic.cs
@@ -1,4 +1,18 @@
-﻿public class CustomHeuristic
-{
+﻿using Framework;
 
+public class CustomHeuristic(string ip, string port, bool useHTTPS) : LoadBalancerHeuristic(ip, port, useHTTPS)
+{
+	private readonly ulong id = GetId();
+
+	public override ulong Invoke()
+	{
+		return id;
+	}
+
+	private static ulong currentId = 0;
+
+	private static ulong GetId()
+	{
+		return Interlocked.Add(ref currentId, 1);
+	}
 }
