c41ea09c47b6ba2f2d6331c0a25b55e36235d085
LazyPanda07 <semengricenko@gmail.com>
2025-12-21 01:27:45 +0500

    Fix

--- DistributionTests/python/run_python_test.sh
+++ DistributionTests/python/run_python_test.sh
@@ -2,9 +2,7 @@
 
 set -e
 
-export LD_LIBRARY_PATH=/usr/local/lib/python3.${PYTHON_MAJOR_VERSION}t/site-packages/web_framework_api/lib:${LD_LIBRARY_PATH}
-
-python3 main.py # download libraries
+export LD_LIBRARY_PATH=/usr/local/lib/python3.${PYTHON_MAJOR_VERSION}/site-packages:${LD_LIBRARY_PATH}
 
 python3 main.py & sleep 1
 
