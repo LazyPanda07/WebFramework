39902c23c9aa2388f8709e88f61f6d4d6f614c04
LazyPanda07 <semengricenko@gmail.com>
2026-03-05 13:34:19 +0500

    Fix

--- API/CC_API/src/dll_handler.c
+++ API/CC_API/src/dll_handler.c
@@ -153,16 +153,16 @@ void wf_delete_web_framework_exception(web_framework_exception_t exception)
 
 void wf_delete_json_builder(json_builder_t builder)
 {
-	typedef void (*wf_delete_json_builder)(void* builder);
+	typedef void (*deleteWebFrameworkJsonBuilder)(void* builder);
 
-	CALL_WEB_FRAMEWORK_FUNCTION(wf_delete_json_builder, builder);
+	CALL_WEB_FRAMEWORK_FUNCTION(deleteWebFrameworkJsonBuilder, builder);
 }
 
 void wf_delete_json_parser(json_parser_t parser)
 {
-	typedef void (*wf_delete_json_parser)(void* parser);
+	typedef void (*deleteWebFrameworkJsonParser)(void* parser);
 
-	CALL_WEB_FRAMEWORK_FUNCTION(wf_delete_json_parser, parser);
+	CALL_WEB_FRAMEWORK_FUNCTION(deleteWebFrameworkJsonParser, parser);
 }
 
 const char* wf_get_data_from_string(web_framework_string_t string)
