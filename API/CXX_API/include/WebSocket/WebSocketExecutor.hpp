564f159520a95e27d55a3dff8d117b7654de5748
LazyPanda07 <semengricenko@gmail.com>
2026-04-30 01:00:49 +0500

    Finish partial implementation of WebSocketExecutor in C++ API

--- API/CXX_API/include/WebSocket/WebSocketExecutor.hpp
+++ API/CXX_API/include/WebSocket/WebSocketExecutor.hpp
@@ -1,8 +1,9 @@
 #pragma once
 
 #include <span>
+#include <variant>
 
-#include "../DLLHandler.hpp"
+#include "DLLHandler.hpp"
 
 namespace framework
 {
@@ -25,11 +26,11 @@ namespace framework
 			Frame(void* implementation);
 
 			template<typename T = char>
-			std::span<T> getPayload() const;
+			std::span<T> getPayload() const requires(std::same_as<T, char> || std::same_as<T, uint8_t>);
 
 			Type getType() const;
 
-			operator bool() const;
+			explicit operator bool() const;
 
 			~Frame() = default;
 		};
@@ -37,9 +38,7 @@ namespace framework
 	public:
 		WebSocketExecutor() = default;
 
-		virtual void send(const Frame& frame) = 0;
-
-		virtual void onReceive(const Frame& frame) = 0;
+		virtual std::variant<std::string, std::vector<uint8_t>> onReceive(const Frame& frame) = 0;
 
 		virtual ~WebSocketExecutor() = default;
 	};
@@ -54,7 +53,7 @@ namespace framework
 	}
 
 	template<typename T>
-	std::span<T> WebSocketExecutor::Frame::getPayload() const
+	std::span<T> WebSocketExecutor::Frame::getPayload() const requires(std::same_as<T, char> || std::same_as<T, uint8_t>)
 	{
 		DEFINE_CLASS_MEMBER_FUNCTION(getFramePayload, char*, uint64_t*, void** exception);
 		void* exception = nullptr;
@@ -90,18 +89,37 @@ namespace framework
 }
 
 #pragma region ExportFunctions
-WEB_FRAMEWORK_FUNCTIONS_API inline void webFrameworkCXXWebSocketExecutorSend(void* implementation, void* frame)
+WEB_FRAMEWORK_FUNCTIONS_API inline void webFrameworkCXXWebSocketExecutorOnReceive(void* implementation, void* frame, void(*sendData)(const uint8_t* data, size_t size, int32_t type))
 {
 	framework::WebSocketExecutor::Frame frameWrapper(frame);
 
-	static_cast<framework::WebSocketExecutor*>(implementation)->send(frameWrapper);
-}
+	std::variant<std::string, std::vector<uint8_t>> data = static_cast<framework::WebSocketExecutor*>(implementation)->onReceive(frameWrapper);
+	const uint8_t* ptr = nullptr;
+	size_t size = 0;
+	framework::WebSocketExecutor::Frame::Type type = framework::WebSocketExecutor::Frame::Type::binary;
 
-WEB_FRAMEWORK_FUNCTIONS_API inline void webFrameworkCXXWebSocketExecutorOnReceive(void* implementation, void* frame)
-{
-	framework::WebSocketExecutor::Frame frameWrapper(frame);
+	if (std::holds_alternative<std::string>(data))
+	{
+		const std::string& temp = std::get<std::string>(data);
 
-	static_cast<framework::WebSocketExecutor*>(implementation)->onReceive(frameWrapper);
+		ptr = reinterpret_cast<const uint8_t*>(temp.data());
+		size = temp.size();
+		type = framework::WebSocketExecutor::Frame::Type::text;
+	}
+	else if (std::holds_alternative<std::vector<uint8_t>>(data))
+	{
+		const std::vector<uint8_t>& temp = std::get<std::vector<uint8_t>>(data);
+
+		ptr = reinterpret_cast<const uint8_t*>(temp.data());
+		size = temp.size();
+		type = framework::WebSocketExecutor::Frame::Type::binary;
+	}
+	else
+	{
+		throw std::runtime_error("Wrong frame send variant type");
+	}
+
+	sendData(ptr, size, static_cast<int32_t>(type));
 }
 
 WEB_FRAMEWORK_FUNCTIONS_API inline void webFrameworkCXXDeleteWebSocketExecutor(void* implementation)
