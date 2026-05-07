314fa1895c156578f97decf2ed5b98456f9c5a79
LazyPanda07 <semengricenko@gmail.com>
2026-05-07 14:00:34 +0500

    Update versions

--- WebFramework/src/C_API/WebFrameworkCApi.cpp
+++ WebFramework/src/C_API/WebFrameworkCApi.cpp
@@ -266,7 +266,7 @@ bool isServerRunning(WebFramework server, Exception* exception)
 
 const char* getWebFrameworkVersion()
 {
-	constexpr std::string_view version = "3.3.7";
+	constexpr std::string_view version = "3.4.0";
 
 	return version.data();
 }
