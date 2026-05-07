f9bc036fb9625e7ae16e26246183dcbfb20269ae
LazyPanda07 <semengricenko@gmail.com>
2026-05-08 04:25:23 +0500

    Add run_csharp_test_arm.sh

---
+++ DistributionTests/csharp/run_csharp_test_arm.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -e
+
+export LD_LIBRARY_PATH=$(pwd)/runtimes/linux-arm64/native:${LD_LIBRARY_PATH}
+
+./csharp_test & sleep 1
+
+while [ ! -f "start.txt" ]; do
+    sleep 1
+done
+
+curl http://127.0.0.1:8080 > out.json
+
+md5sum reference.json
+md5sum out.json
+
+diff reference.json out.json
