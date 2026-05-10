f38803f919ec304be74dc4a35471ae1661cca0f3
LazyPanda07 <semengricenko@gmail.com>
2026-05-11 01:29:50 +0500

    Fix

--- API/CC_API/src/json_parser.c
+++ API/CC_API/src/json_parser.c
@@ -243,16 +243,16 @@ bool wf_try_get_json_parser_array(json_parser_t implementation, const char* key,
 	return result;
 }
 
-web_framework_exception_t* wf_get_parsed_data(json_parser_t implementation, json_object_t* result, bool weak)
+web_framework_exception_t* wf_get_json_parser_parsed_data(json_parser_t implementation, json_object_t* result, bool weak)
 {
 	typedef void* (*getJsonParserParsedData)(void* implementation, bool weak, void** exception);
 	void* exception = NULL;
 
-	void* result = CALL_CLASS_MEMBER_WEB_FRAMEWORK_FUNCTION(getJsonParserParsedData, weak, &exception);
+	void* temp = CALL_CLASS_MEMBER_WEB_FRAMEWORK_FUNCTION(getJsonParserParsedData, weak, &exception);
 
 	*result = (json_object_t)
 	{
-		.implementation = result,
+		.implementation = temp,
 		.weak = weak
 	};
 
